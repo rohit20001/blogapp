9310f58d72ef4ba9b5aa0128369e1a50d9943f1a
rohit verma <rohit9721382965@gmail.com>
2020-10-29 22:38:07 +0530

    added

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from main.models import *
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+     list_display = ('title','posted_date')
+
+
+admin.site.register(Post, PostAdmin)
+
